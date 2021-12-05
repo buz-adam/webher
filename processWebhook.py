2eae841d45555e283e40f930bb8e0dc3c816e6c3
dtekeli <49680162+buz-adam@users.noreply.github.com>
2021-12-05 22:12:36 +0300

    news

--- processWebhook.py
+++ processWebhook.py
@@ -1,18 +1,14 @@
 import flask
 import os
-from flask import send_from_directory
+from flask import send_from_directory, render_template
 
 app = flask.Flask(__name__)
 
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/favicon.png')
 
 @app.route('/')
-@app.route('/home')
-def home():
-    return "Hello World"
+@app.route('/index.html')
+def index():
+    return render_template('index.html', the_title='Tiger Home Page')
 
 if __name__ == "__main__":
     app.secret_key = 'ItIsASecret'
