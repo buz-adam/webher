6807b92e797d5d45a464cd6088826c6c5452c750
dtekeli <49680162+buz-adam@users.noreply.github.com>
2021-12-06 21:12:36 +0300

    new

--- processWebhook.py
+++ processWebhook.py
@@ -6,13 +6,12 @@ app = flask.Flask(__name__)
 
 @app.route('/favicon.ico')
 def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/favicon.png')
+    return send_from_directory(os.path.join(app.root_path, 'static'),'favicon.ico', mimetype='image/favicon.png')
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return '<html lang="en"> <head><meta charset="UTF-8"><title>FlaskApp</title></head><body> <img src="https://ichi.pro/assets/images/max/724/0*hVrbNMq2F7OkXVnE" alt="alternatetext">  <h1>Merhaba DÜNYA!</h1><h2>HerokuAPP!</h2></body></html>'
+    return '<html lang="en"> <head><meta charset="UTF-8"><title>FlaskApp</title></head><body> <img src="https://ichi.pro/assets/images/max/724/0*hVrbNMq2F7OkXVnE" alt="alternatetext">  <h1>Merhaba DÜNYA!</h1><h2>HerokuAPP!</h2><a href="https://heroku.com/">->heroku</a></body></html>'
 
 if __name__ == "__main__":
     app.secret_key = 'ItIsASecret'
