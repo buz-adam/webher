c9f42558b38575f1f7315dcde341ab3a4a84fefe
dtekeli <49680162+buz-adam@users.noreply.github.com>
2021-12-06 20:58:22 +0300

    qw

--- processWebhook.py
+++ processWebhook.py
@@ -12,7 +12,7 @@ def favicon():
 @app.route('/')
 @app.route('/home')
 def home():
-    return '<html lang="en"> <head><meta charset="UTF-8"><title>FlaskApp</title></head><body> <img src="url" alt="alternatetext">  <h1>Merhaba DÜNYA!</h1><h2>HerokuAPP!</h2></body></html>'
+    return '<html lang="en"> <head><meta charset="UTF-8"><title>FlaskApp</title></head><body> <img src="https://ichi.pro/assets/images/max/724/0*hVrbNMq2F7OkXVnE" alt="alternatetext">  <h1>Merhaba DÜNYA!</h1><h2>HerokuAPP!</h2></body></html>'
 
 if __name__ == "__main__":
     app.secret_key = 'ItIsASecret'
