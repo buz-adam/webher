b51c80ae73b295f4ccdd8716eb6c1e28933de975
dtekeli <49680162+buz-adam@users.noreply.github.com>
2021-12-06 20:41:54 +0300

    new hello world

--- processWebhook.py
+++ processWebhook.py
@@ -12,7 +12,7 @@ def favicon():
 @app.route('/')
 @app.route('/home')
 def home():
-    return '<html lang="en"> <head><meta charset="UTF-8"><title>FlaskApp</title></head><body><h1>Hello World!</h1><h2>FlaskApp!</h2></body></html>'
+    return '<html lang="en"> <head><meta charset="UTF-8"><title>FlaskApp</title></head><body><h1>Merhaba DÜNYA!</h1><h2>FlaskApp!</h2></body></html>'
 
 if __name__ == "__main__":
     app.secret_key = 'ItIsASecret'
